39ea0f3ef10747e3ff7da518a655ba55820f8df0
KarinaMealey <kem32002@gmail.com>
2018-06-19 15:01:39 +0000

    automatically backed up by learn

--- argument.rb
+++ argument.rb
@@ -2,4 +2,4 @@ def greeting(local_greeting, your_name, my_name, time_of_day)
   puts "#{local_greeting} #{your_name}, I am #{my_name} and the time is #{time_of_day}"
 end
 
-greeting ("Howdy","Esmerelda","Karina","9:56")
+greeting ("Howdy", "Esmerelda", "Karina", "9:56")
